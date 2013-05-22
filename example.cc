ab17ce800ab514dc44ba6df3c3d59528185b92aa
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-05-23 01:12:17 +0200

    move some code

--- example.cc
+++ example.cc
@@ -1,34 +1,14 @@
 #include <memory>
-#include <lart/ringbuffer.h>
 #include <iostream>
 
-struct op_base 
-{
-	virtual ~op_base() { }
-	virtual void exec() = 0;
-};
+#include <lart/ringbuffer.h>
+#include <lart/op.h>
 
-typedef std::shared_ptr<op_base> op_base_ptr;
-
-template<class T> 
-struct op : op_base
-{
-	T m_t;
-	op(T t) : m_t(t) { }
-	virtual void exec() { m_t(); }
-};
-
-
-template<class T>
-op_base_ptr make_op(T t)
-{
-	return op_base_ptr(new op<T>(t));
-}
 
 int main()
 {
-	auto t = make_op([]() { std::cout << "hi" << std::endl; });
-	auto rb = ringbuffer<op_base_ptr>(10);
+	auto t = lart::make_op([]() { std::cout << "hi" << std::endl; });
+	auto rb = ringbuffer<lart::op_base_ptr>(10);
 	rb.write(t);
 
 	auto t2 = rb.read();
