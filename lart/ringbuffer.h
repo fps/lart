3b53d0b5ff114fe21cb19946f4bd4d2d7f4b4a9c
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-05-26 15:13:59 +0200

    put ringbuffer in namespace, too

--- lart/ringbuffer.h
+++ lart/ringbuffer.h
@@ -4,78 +4,82 @@
 #include <jack/ringbuffer.h>
 #include <memory>
 
-/**
-	T needs to be a default constructable type.. And it has to have valid
-	copy constructor/assignment operator
+namespace lart
+{
 
-	Note that this class needs to construct n objects of type T (with n == size)  so 
-	that the places in the ringbuffer become assignable
+	/**
+		T needs to be a default constructable type.. And it has to have valid
+		copy constructor/assignment operator
 
-	Note that the objects remain in the ringbuffer until they are overwritten again
-	when the ringbuffer returns to the current position the next time around. I.e. a
-	read() does not assign a T() to the read object as that could cause destructors
-	to be called, etc..
+		Note that this class needs to construct n objects of type T (with n == size)  so 
+		that the places in the ringbuffer become assignable
 
-	Note that read() creates a copy of the T, so the T(const T&) should be non blocking
-*/
-template <class T> 
-struct ringbuffer {
-	unsigned int size;
+		Note that the objects remain in the ringbuffer until they are overwritten again
+		when the ringbuffer returns to the current position the next time around. I.e. a
+		read() does not assign a T() to the read object as that could cause destructors
+		to be called, etc..
 
-	jack_ringbuffer_t *jack_ringbuffer;
+		Note that read() creates a copy of the T, so the T(const T&) should be non blocking
+	*/
+	template <class T> 
+	struct ringbuffer {
+		unsigned int size;
 
-	ringbuffer(unsigned int size) : size(size) {
-		jack_ringbuffer = jack_ringbuffer_create(sizeof(T) * size);
+		jack_ringbuffer_t *jack_ringbuffer;
 
-		for (unsigned int i = 0; i < size; ++i) {
-			new (jack_ringbuffer->buf + sizeof(T) * i) T();
-		}
-	}
+		ringbuffer(unsigned int size) : size(size) {
+			jack_ringbuffer = jack_ringbuffer_create(sizeof(T) * size);
 
-	~ringbuffer() {
-		for (unsigned int i = 0; i < size; ++i) {
-			((T*)(jack_ringbuffer->buf + sizeof(T) * i))->~T();
+			for (unsigned int i = 0; i < size; ++i) {
+				new (jack_ringbuffer->buf + sizeof(T) * i) T();
+			}
 		}
 
-		jack_ringbuffer_free(jack_ringbuffer);
-	}
+		~ringbuffer() {
+			for (unsigned int i = 0; i < size; ++i) {
+				((T*)(jack_ringbuffer->buf + sizeof(T) * i))->~T();
+			}
 
-	bool can_write() {
-		if (jack_ringbuffer_write_space(jack_ringbuffer) >= sizeof(T)) {
-			return true;
+			jack_ringbuffer_free(jack_ringbuffer);
 		}
 
-		return false;
-	}
+		bool can_write() {
+			if (jack_ringbuffer_write_space(jack_ringbuffer) >= sizeof(T)) {
+				return true;
+			}
 
-	void write(const T &t) {
-		jack_ringbuffer_data_t rb_data[2];
-		jack_ringbuffer_get_write_vector(jack_ringbuffer, rb_data);
-		*((T*)rb_data->buf) = t;
-		jack_ringbuffer_write_advance(jack_ringbuffer, sizeof(T));
-	}
-
-	bool can_read() {
-		if (jack_ringbuffer_read_space(jack_ringbuffer) >= sizeof(T)) {
-			return true;
+			return false;
 		}
 
-		return false;
-	}
+		void write(const T &t) {
+			jack_ringbuffer_data_t rb_data[2];
+			jack_ringbuffer_get_write_vector(jack_ringbuffer, rb_data);
+			*((T*)rb_data->buf) = t;
+			jack_ringbuffer_write_advance(jack_ringbuffer, sizeof(T));
+		}
 
-	T read() {
-		jack_ringbuffer_data_t rb_data[2];
-		jack_ringbuffer_get_read_vector(jack_ringbuffer, rb_data);
-		T t = *((T*)rb_data->buf);
-		jack_ringbuffer_read_advance(jack_ringbuffer, sizeof(T));
-		return t;
-	}
+		bool can_read() {
+			if (jack_ringbuffer_read_space(jack_ringbuffer) >= sizeof(T)) {
+				return true;
+			}
 
-	T& snoop() {
-		jack_ringbuffer_data_t rb_data[2];
-		jack_ringbuffer_get_read_vector(jack_ringbuffer, rb_data);
-		return *((T*)rb_data->buf);
-	}
-};
+			return false;
+		}
+
+		T read() {
+			jack_ringbuffer_data_t rb_data[2];
+			jack_ringbuffer_get_read_vector(jack_ringbuffer, rb_data);
+			T t = *((T*)rb_data->buf);
+			jack_ringbuffer_read_advance(jack_ringbuffer, sizeof(T));
+			return t;
+		}
+
+		T& snoop() {
+			jack_ringbuffer_data_t rb_data[2];
+			jack_ringbuffer_get_read_vector(jack_ringbuffer, rb_data);
+			return *((T*)rb_data->buf);
+		}
+	};
+}
 
 #endif
